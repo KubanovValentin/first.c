216971f5164304509837fb0875af5a5cca53e78f
Valentin <79686793+KubanovValentin@users.noreply.github.com>
2022-04-18 18:29:02 +0000

    1

--- main.c
+++ main.c
@@ -2,13 +2,35 @@
 #include "cs50.h"
 
 int main(void) {
-  
  // string answer = get_string("Как тебя зовут? ");
  //  printf("Привет, %s\n",answer);
-
+{
   int x=get_int("x: ");
   int y=get_int("y: ");
 
   printf("%i\n",x+y);
+  
+}
+
+{
+  int x=2;
+  int y=3;
+  printf("%i\n",x+y);
+}
+  {
+    int x=get_int("x: ");
+    int y=get_int("y: ");
+
+    if(x<y){
+      printf(" x ");
+    }
+    float z=(float)x/(float)y;
+    printf("%f\n",z);
+  }
+  {
+    for(int i=0;i<3;i++){
+      printf("myau");
+    }
+  }
 }
 
