7241f34b94ba85bfb297073c50ce6cb82bde54c0
Валентин Кубанов <>
2022-04-18 18:00:44 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@MediumpurpleTepidAbstraction/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "cs50.h"
+
+int main(void) {
+  
+ // string answer = get_string("Как тебя зовут? ");
+ //  printf("Привет, %s\n",answer);
+
+  int x=get_int("x: ");
+  int y=get_int("y: ");
+
+  printf("%i\n",x+y);
+}
+
---
+++ cs50.d
@@ -0,0 +1,1 @@
+cs50.o cs50.d : cs50.c cs50.h
